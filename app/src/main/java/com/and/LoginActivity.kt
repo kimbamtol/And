f9963797ec33c7725bcf44daf93717718ebda79b
hoijun <65825603+hoijun@users.noreply.github.com>
2024-04-19 12:51:33 +0900

    로그인 수정
    로그인 수정

--- app/src/main/java/com/and/LoginActivity.kt
+++ app/src/main/java/com/and/LoginActivity.kt
@@ -22,12 +22,14 @@ class LoginActivity : AppCompatActivity() {
         _binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /** HashKey확인 */
+        // HashKey확인
         val keyHash = Utility.getKeyHash(this)
-        Log.e("Key", "keyHash: ${keyHash}")
+        Log.e("Key", "keyHash: $keyHash")
 
+        // Kakao SDK 초기화
         KakaoSdk.init(this, this.getString(R.string.kakao_app_key))
 
+        // 로그인 버튼 클릭 리스너 설정
         binding.btnStartKakaoLogin.setOnClickListener {
             kakaoLogin()
         }
@@ -35,43 +37,33 @@ class LoginActivity : AppCompatActivity() {
 
     private fun kakaoLogin() {
         // 카카오계정으로 로그인 공통 callback 구성
-        // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        val callback: (OAuthToken?, Throwable?) -> Unit = callback@{ token, error ->
             if (error != null) {
-                setLogin(false)
-            } else if (token != null) {
-                //TODO: 최종적으로 카카오로그인 및 유저정보 가져온 결과
-                UserApiClient.instance.me { user, error ->
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    setLogin(true)
+                if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    return@callback // 로그인 취소 처리
                 }
+                setLogin(false) // 로그인 실패 처리
+            } else if (token != null) {
+                updateUIAndFinish() // 로그인 성공 후 UI 업데이트 및 MainActivity로 이동
             }
         }
 
-        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 그렇지 않으면 카카오계정으로 로그인 시도
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                if (error != null) {
-
-                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        return@loginWithKakaoTalk
-                    }
-
-                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
-                    UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
-                } else if (token != null) {
-                    setLogin(true)
-                }
-            }
+            UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
         } else {
             UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
         }
     }
 
-    private fun setLogin(bool: Boolean){
-        binding.btnStartKakaoLogin.visibility = if(bool) View.GONE else View.VISIBLE
+    private fun updateUIAndFinish() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish() // LoginActivity를 액티비티 스택에서 제거
+        setLogin(true) // 로그인 버튼을 숨김
+    }
+
+    private fun setLogin(bool: Boolean) {
+        binding.btnStartKakaoLogin.visibility = if (bool) View.GONE else View.VISIBLE
     }
 }
